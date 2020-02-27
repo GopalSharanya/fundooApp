485439fd268ad0f525cc5412667596f3b745a8a4
sharanya gopal <sharanyagopal97@gmail.com>
2020-02-27 12:24:40 +0530

    [sharanya]Modified. modified note controller

--- controller/noteController.js
+++ controller/noteController.js
@@ -12,65 +12,24 @@ var scheduler = require('../middleware/reminder')
  * @param {res}- response to be sent back to client
  */
 
-try{
-exports.addNote = (req, res) => {
-    req.checkBody("discription", "enter discription ").notEmpty();
-    req.checkBody("title", "title not entered").notEmpty();
+try {
+    exports.addNote = (req, res) => {
+        req.checkBody("discription", "enter discription ").notEmpty();
+        req.checkBody("title", "title not entered").notEmpty();
 
-    var error = req.validationErrors();
-    var response = {};
+        var error = req.validationErrors();
+        var response = {};
 
-    if (error) {
-        response.data = error;
-        response.sucess = false;
-        res.status(422).send(response);
-    }
-
-    else {
-        service.addNote(req)
-            .then((data) => {
-                redis.redisdeleteNotes(req.decoded.payload.user_id)
-                response.data = data;
-                response.sucess = true;
-                res.status(200).send(response);
-            })
-            .catch((err) => {
-                response.data = err;
-                response.sucess = false;
-                res.status(422).send(response);
-            })
-    }
-}
-}
-catch(err){
-    console.log(err);
-}
-
-/**
- * @module getNotes
- * @param {req} - request from user to show back all the registered notes
- * @param {res}- response to be sent back to client
- */
-
-
- try{
-exports.getNotes = (req, res) => {
-    var id = {
-        userId: req.decoded.payload.user_id
-    }
-
-    var response = {};
-
-    redis.redisgetNotes(id.userId, (err, data) => {     //if the notes are present in chache memory itll be retrived back from chache
-        if (data) {
-            response.data = data;
-            response.sucess = true;
-            res.status(200).send(response);
+        if (error) {
+            response.data = error;
+            response.sucess = false;
+            res.status(422).send(response);
         }
+
         else {
-            service.getNotes(id)
+            service.addNote(req)
                 .then((data) => {
-                    redis.redissetNotes(JSON.stringify(data), id.userId)
+                    redis.redisdeleteNotes(req.decoded.payload.user_id)
                     response.data = data;
                     response.sucess = true;
                     res.status(200).send(response);
@@ -81,12 +40,53 @@ exports.getNotes = (req, res) => {
                     res.status(422).send(response);
                 })
         }
-    })
+    }
 }
- }
- catch(err){
-     console.log(err);
- }
+catch (err) {
+    console.log(err);
+}
+
+/**
+ * @module getNotes
+ * @param {req} - request from user to show back all the registered notes
+ * @param {res}- response to be sent back to client
+ */
+
+
+try {
+    exports.getNotes = (req, res) => {
+        var id = {
+            userId: req.decoded.payload.user_id
+        }
+
+        var response = {};
+
+        redis.redisgetNotes(id.userId, (err, data) => {     //if the notes are present in chache memory itll be retrived back from chache
+            if (data) {
+                response.data = data;
+                response.sucess = true;
+                res.status(200).send(response);
+            }
+            else {
+                service.getNotes(id)
+                    .then((data) => {
+                        redis.redissetNotes(JSON.stringify(data), id.userId)
+                        response.data = data;
+                        response.sucess = true;
+                        res.status(200).send(response);
+                    })
+                    .catch((err) => {
+                        response.data = err;
+                        response.sucess = false;
+                        res.status(422).send(response);
+                    })
+            }
+        })
+    }
+}
+catch (err) {
+    console.log(err);
+}
 
 
 /**
@@ -95,39 +95,39 @@ exports.getNotes = (req, res) => {
  * @param {res}- response to be sent back to client
  */
 
- try{
-exports.updateNotes = (req, res) => {
+try {
+    exports.updateNotes = (req, res) => {
 
-    req.checkBody("title", "title cant be empty").notEmpty();
-    req.checkBody("discription", "discription cant be empty").notEmpty();
+        req.checkBody("title", "title cant be empty").notEmpty();
+        req.checkBody("discription", "discription cant be empty").notEmpty();
 
-    var response = {};
-    var error = req.validationErrors();
+        var response = {};
+        var error = req.validationErrors();
 
-    if (error) {
-        response.data = error;
-        response.sucess = false;
-        res.status(422).send(response);
-    }
+        if (error) {
+            response.data = error;
+            response.sucess = false;
+            res.status(422).send(response);
+        }
 
-    else {
-        service.updateNotes(req)
-            .then((data) => {
-                response.data = data;
-                response.sucess = true;
-                res.status(200).send(response);
-            })
-            .catch((err) => {
-                response.data = err;
-                response.sucess = false;
-                res.status(422).send(response);
-            })
+        else {
+            service.updateNotes(req)
+                .then((data) => {
+                    response.data = data;
+                    response.sucess = true;
+                    res.status(200).send(response);
+                })
+                .catch((err) => {
+                    response.data = err;
+                    response.sucess = false;
+                    res.status(422).send(response);
+                })
+        }
     }
 }
- }
- catch(err){
-     console.log(err);
- }
+catch (err) {
+    console.log(err);
+}
 /**
  * @module deleteNote
  * @param {req} - request from user contains note information to be deleted
@@ -156,27 +156,27 @@ exports.deleteNote = (req, res) => {
  * @param {res}- response to be sent back to client
  */
 
- try{
-exports.imageUpload = (req, res) => {
-    var imageUrl = req.file.location;
-    var response = {};
+try {
+    exports.imageUpload = (req, res) => {
+        var imageUrl = req.file.location;
+        var response = {};
 
-    service.imageUpload(req, imageUrl)
-        .then((data) => {
-            response.data = data;
-            response.sucess = true;
-            res.status(200).send(response);
-        })
-        .catch((err) => {
-            response.data = err;
-            response.sucess = false;
-            res.status(422).send(response);
-        })
+        service.imageUpload(req, imageUrl)
+            .then((data) => {
+                response.data = data;
+                response.sucess = true;
+                res.status(200).send(response);
+            })
+            .catch((err) => {
+                response.data = err;
+                response.sucess = false;
+                res.status(422).send(response);
+            })
+    }
 }
- }
- catch(err){
-     console.log(err);
- }
+catch (err) {
+    console.log(err);
+}
 
 /**
  * @module collabAdd
@@ -184,23 +184,28 @@ exports.imageUpload = (req, res) => {
  * @param {res}- response to be sent back to client
  */
 
-exports.collabAdd = (req, res) => {
-    var response = {};
-    if (req.noteId === req.decoded.payload.user_id) {
-        response.data = " invalid id";
-        response.sucess = false
-        res.status(422).send(response);
-    }
+try {
+    exports.collabAdd = (req, res) => {
+        var response = {};
+        if (req.noteId === req.decoded.payload.user_id) {
+            response.data = " invalid id";
+            response.sucess = false
+            res.status(422).send(response);
+        }
 
-    else {
-        service.collabAdd(req)
-            .then((data) => {
-                response.data = data;
-                response.sucess = true;
-                res.status(200).send(response)
-            }
-            )
+        else {
+            service.collabAdd(req)
+                .then((data) => {
+                    response.data = data;
+                    response.sucess = true;
+                    res.status(200).send(response)
+                }
+                )
+        }
     }
+}
+catch (err) {
+    console.log(err)
 }
 
 /**
